bd40eb3c80317d8771f7a63d10473f178b2b814e
HemanthKumar-CN <hemanth.html+github@gmail.com>
2022-05-18 13:35:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,17 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <h2>LOGOBAKERY</h2>
+      <div  className="dis">
+      <a href="">Services</a>
+      <a href="">Projects</a>
+      <a href="">About</a>
+      </div>
+      <button className="button">Contact</button>
+
+
     </div>
   );
 }
